db8203d0892c3e81d39ffc935a656bbd80d0df4a
jaluik <cqzhengmouren@gmail.com>
2020-08-20 17:28:31 +0800

    feat: 配置devserver

--- scripts/config/webpack.common.js
+++ scripts/config/webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { PROJECT_PATH, isDev } = require('../constants');
 
 module.exports = {
@@ -9,4 +10,27 @@ module.exports = {
     filename: `js/[name]${isDev ? '' : '.[hash:8]'}.js`,
     path: path.resolve(PROJECT_PATH, './dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(PROJECT_PATH, 'public/index.html'),
+      filename: 'index.html',
+      cache: false,
+      minify: isDev
+        ? false
+        : {
+            removeAttributeQuotes: true,
+            collapseWhitespace: true,
+            removeComments: true,
+            collapseBooleanAttributes: true,
+            collapseInlineTagWhitespace: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            minifyCSS: true,
+            minifyJS: true,
+            minifyURLs: true,
+            useShortDoctype: true,
+          },
+    }),
+  ],
 };
--- scripts/config/webpack.development.js
+++ scripts/config/webpack.development.js
@@ -1,6 +1,16 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const { SERVER_HOST, SERVER_PORT } = require('../constants');
 
 module.exports = merge(common, {
   mode: 'development',
+  devServer: {
+    host: SERVER_HOST,
+    port: SERVER_PORT,
+    open: true,
+    hot: true,
+    stats: 'errors-only',
+    compress: true,
+    clientLogLevel: 'silent',
+  },
 });
